32b9041478174aeeccc03a760cb4f0779be43b3f
Matt Landreman <matt.landreman@typeoneenergy.com>
2024-07-24 18:11:45 -0400

    mean_k_parallel working

--- gx_features/calculations.py
+++ gx_features/calculations.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+
 def differentiate(data):
     """Apply d/dz to the data, assuming periodic boundary conditions.
-    
+
     Centered finite differences are used rather than a spectral method,
     due to the discontinuities in gds21 at the domain boundary.
 
@@ -11,3 +12,30 @@ def differentiate(data):
     n_z = data.shape[1]
     dz = 2 * np.pi / n_z
     return (np.roll(data, -1, axis=1) - np.roll(data, 1, axis=1)) / (2 * dz)
+
+
+def compute_mean_k_parallel(data):
+    """Compute the mean k_parallel for each data point.
+
+    data should have shape (n_data, n_z, n_quantities).
+    """
+    n_data, n_z, n_quantities = data.shape
+
+    # Compute the Fourier transform of the data
+    data_hat = np.fft.fft(data, axis=1)
+
+    # Extract only the positive frequencies. Negative frequencies just have
+    # amplitudes that are complex conjugates of the positive frequencies. Also
+    # we are not interested in the DC component.
+    max_index = n_z // 2
+    data_hat = data_hat[:, 1:max_index, :]
+    # k_parallel = k_parallel[1:max_index]
+    k_parallel = np.arange(1, max_index)
+
+    data_hat_abs2 = np.abs(data_hat) ** 2
+    mean_k_parallel = np.sum(k_parallel[None, :, None] * data_hat_abs2, axis=1) / np.sum(data_hat_abs2, axis=1)
+
+    # To avoid NaNs:
+    assert np.min(np.sum(data_hat_abs2, axis=1)) > 0, "Some row of data was constant so mean k|| is not defined."
+
+    return mean_k_parallel
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from gx_features.calculations import differentiate
+from gx_features.calculations import differentiate, compute_mean_k_parallel
 
 class Tests(unittest.TestCase):
     def test_differentiate(self):
@@ -21,4 +21,42 @@ class Tests(unittest.TestCase):
                 feature_tensor[j_data, :, 1],
                 3 * j_data * np.cos(j_data * z + 1),
                 rtol=3e-2,
+            )
+
+    def test_mean_k_parallel(self):
+        # First try a pure frequency:
+
+        n_data = 4
+        n_quantities = 2
+        n_z = 96
+        # n_z = 16
+        z = np.linspace(-np.pi, np.pi, n_z, endpoint=False)
+
+        feature_tensor = np.zeros((n_data, n_z, n_quantities))
+        for j_data in range(n_data):
+            feature_tensor[j_data, :, 0] = 3 * np.sin((j_data + 1) * z + 1)
+            feature_tensor[j_data, :, 1] = 17 * np.cos((j_data + 1) * z - 1)
+
+        features = compute_mean_k_parallel(feature_tensor)
+
+        for j_data in range(n_data):
+            np.testing.assert_allclose(
+                features[j_data, :],
+                j_data + 1, 
+                rtol=1e-14,
+            )
+
+        # Now try a sum of frequencies:
+        
+        for j_data in range(n_data):
+            feature_tensor[j_data, :, 0] = 3 * (np.sin((j_data + 1) * z + 1) + np.sin((j_data + 2) * z + j_data))
+            feature_tensor[j_data, :, 1] = 17 * (np.cos((j_data + 1) * z - 1) + np.cos((j_data + 2) * z - j_data))
+
+        features = compute_mean_k_parallel(feature_tensor)
+
+        for j_data in range(n_data):
+            np.testing.assert_allclose(
+                features[j_data, :],
+                j_data + 1.5,
+                rtol=1e-14,
             )
